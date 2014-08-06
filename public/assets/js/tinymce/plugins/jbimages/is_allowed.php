29b5c6fffb8535cbc366f56b04f442a37f8d8628
Ryan Robinson <ryrobbo@gmail.com>
2014-08-06 22:00:43 +0100

    Added security to image uploader

--- public/assets/js/tinymce/plugins/jbimages/is_allowed.php
+++ public/assets/js/tinymce/plugins/jbimages/is_allowed.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Justboil.me - a TinyMCE image upload plugin
  * jbimages/config.php
@@ -11,22 +12,24 @@
  *
  * Version: 2.3 released 23/06/2013
  */
- 
- 
-/*-------------------------------------------------------------------
-|
-| IMPORTANT NOTE! In case, when TinyMCE’s folder is not protected with HTTP Authorisation,
-| you should require is_allowed() function to return 
-| `TRUE` if user is authorised,
-| `FALSE` - otherwise
-| 
-|  This is intended to protect upload script, if someone guesses it's url.
-| 
--------------------------------------------------------------------*/
+/* -------------------------------------------------------------------
+  |
+  | IMPORTANT NOTE! In case, when TinyMCEï¿½s folder is not protected with HTTP Authorisation,
+  | you should require is_allowed() function to return
+  | `TRUE` if user is authorised,
+  | `FALSE` - otherwise
+  |
+  |  This is intended to protect upload script, if someone guesses it's url.
+  |
+  ------------------------------------------------------------------- */
 
-function is_allowed()
-{
-	return TRUE;
+function is_allowed() {
+    session_start();
+    if (isset($_SESSION['admin'])) {
+        return TRUE;
+    } else {
+        return false;
+    }
 }
 
 ?>
